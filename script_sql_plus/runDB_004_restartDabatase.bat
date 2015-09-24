5abdad4aadc1906d1ced0d4311ba2ee86add5e6d
atafs <atafs@iscte-iul.pt>
2015-09-24 19:30:26 +0100

    commit

---
+++ script_sql_plus/runDB_004_restartDabatase.bat
@@ -0,0 +1,12 @@
+
+@rem LINK
+@rem https://www.youtube.com/watch?v=F81gfUnznZY&index=2&list=PLL_LQvNX4xKzVcg_GoRzjeM_veo8CI15A
+
+@rem RESTART DATABASE (CMD)
+sqlplus / as sysdba
+shutdown abort
+startup
+
+@rem EXIT
+exit;
+
